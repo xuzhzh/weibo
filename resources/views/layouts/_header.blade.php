6b89d8f61b51581241e9f488b465da7df4ed1837
vagrant <vagrant@homestead>
2020-05-15 06:47:41 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,8 +2,8 @@
   <div class="container ">
     <a class="navbar-brand" href="/">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-      <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+      <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
+      <li class="nav-item" ><a class="nav-link" href="{{ route('about') }}">登录</a></li>
     </ul>
   </div>
 </nav>
