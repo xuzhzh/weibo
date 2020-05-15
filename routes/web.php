5178f68f26eb119adc9048d144b6e09039669917
vagrant <vagrant@homestead>
2020-05-15 09:18:08 +0000

    用户注册表

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,7 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 
+Route::resource('users', 'UsersController');
+
+
 
