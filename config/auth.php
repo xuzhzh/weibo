ef67ff70f327ff7829f536504cad67a76ce78e41
vagrant <vagrant@homestead>
2020-05-15 07:56:16 +0000

    修改model位置

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Modules\User::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
