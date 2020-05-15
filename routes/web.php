e9ab908174434babf74c5b17f04d86b0f7e5871a
vagrant <vagrant@homestead>
2020-05-15 12:01:26 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,10 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::resource('users', 'UsersController');
 
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+
 
 
