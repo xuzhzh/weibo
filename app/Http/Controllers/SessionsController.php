f99b5db893bd87424bb722a652a81a6fd2c2e84b
vagrant <vagrant@homestead>
2020-05-18 01:21:16 +0000

    ´´½¨»Ự

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,6 +26,6 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
-        
+
     }
 }
