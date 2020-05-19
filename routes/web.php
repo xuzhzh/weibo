84806421306511830eb6adefc1df4ecd1f1c24b6
vagrant <vagrant@homestead>
2020-05-19 07:54:02 +0000

    Óû§¼¤»

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@
 
 Route::get('signup', 'UsersController@create')->name('signup');
 
-Route::get('/','StaticPagesController@home')->name('home');
-Route::get('/help','StaticPagesController@help')->name('help');
-Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
 Route::resource('users', 'UsersController');
@@ -27,6 +27,8 @@ Route::resource('users', 'UsersController');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
 
 
