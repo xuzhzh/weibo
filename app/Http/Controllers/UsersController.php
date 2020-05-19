18b4b0025f7b148a9ede919317ba404a6681480e
vagrant <vagrant@homestead>
2020-05-19 07:28:46 +0000

    ¹ÜÀíԱ¿ÉÒÔɾ³ýÓû§

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -85,4 +85,12 @@ class UsersController extends Controller
         session()->flash('success', '更新成功');
         return redirect()->route('users.show', $user->id);
     }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,10 +10,13 @@ class UserPolicy
     use HandlesAuthorization;
 
 
-    public function update(User $currentUser,User $user)
+    public function update(User $currentUser, User $user)
     {
-
         return $currentUser->id === $user->id;
+    }
 
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
