742a388ed528281e72af7f4cc481b72a7b6df18c
vagrant <vagrant@homestead>
2020-05-19 06:55:26 +0000

    ·ÃÎʲßÂ

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,16 @@ use Auth;
 class UsersController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -27,6 +37,7 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
+
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
@@ -47,11 +58,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(Request $request, User $user)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'password' => 'nullable|confirmed|min:6'
