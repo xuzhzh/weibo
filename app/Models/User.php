892e2965977a2ec9bfe7504679e339f00fe226c1
vagrant <vagrant@homestead>
2020-05-27 06:19:28 +0000

    Ê×ҳ΢²©Áб

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::Class);
     }
+
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
 }
