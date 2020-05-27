28b77dc474f2fdca3bd9481e05afd79b8667bea4
vagrant <vagrant@homestead>
2020-05-27 08:05:06 +0000

    ¹ØעºͷÛ˿ÁбíҳÃ

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,8 +34,8 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        $statuses= $user->statuses()->orderBy('created_at','desc')->paginate(30);
-        return view('users.show', compact('user','statuses'));
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(30);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     /**
@@ -120,5 +120,19 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
     }
 }
